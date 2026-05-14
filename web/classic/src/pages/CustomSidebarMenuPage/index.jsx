c1ca0a219514732cd48fc1c672311616ec663391
chuxin <772545663@qq.com>
2026-05-15 00:01:20 +0800

    Rebrand app chrome to Model-go

--- web/classic/src/pages/CustomSidebarMenuPage/index.jsx
+++ web/classic/src/pages/CustomSidebarMenuPage/index.jsx
@@ -20,8 +20,8 @@ For commercial licensing, please contact support@quantumnous.com
 import React, { useContext, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
-import { Button, Empty } from '@douyinfe/semi-ui';
-import { IconAlertCircle, IconExternalOpen } from '@douyinfe/semi-icons';
+import { Empty } from '@douyinfe/semi-ui';
+import { IconAlertCircle } from '@douyinfe/semi-icons';
 import { StatusContext } from '../../context/Status';
 import { parseCustomSidebarMenus } from '../../helpers/customSidebarMenus';
 
@@ -39,23 +39,6 @@ const CustomSidebarMenuPage = () => {
   return (
     <div className='mt-[64px] h-[calc(100vh-64px)] bg-[#f7fbfb] p-6'>
       <div className='flex h-full flex-col overflow-hidden rounded-xl border border-semi-color-border bg-semi-color-bg-0 shadow-sm'>
-        <div className='flex h-16 flex-shrink-0 items-center justify-between border-b border-semi-color-border px-6'>
-          <h1 className='m-0 text-xl font-semibold text-semi-color-text-0'>
-            {menu?.title || t('自定义菜单')}
-          </h1>
-          {menu?.url && (
-            <Button
-              theme='light'
-              icon={<IconExternalOpen />}
-              onClick={() =>
-                window.open(menu.url, '_blank', 'noopener,noreferrer')
-              }
-            >
-              {t('新窗口打开')}
-            </Button>
-          )}
-        </div>
-
         {menu?.url ? (
           <div className='flex-1 overflow-hidden bg-white'>
             <iframe
